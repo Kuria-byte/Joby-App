b2bc3eeb98d9d7af73d723f12e9b8743aa67c330
Kuria_byte <mwitumi21@gmail.com>
2025-02-10 19:53:46 +0300

    Generatd a service worker registration file that registers the service worker, handles caching of static assets, and listens for updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/global.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
@@ -13,5 +14,22 @@ root.render(
   </React.StrictMode>
 );
 
-// Register service worker for PWA functionality
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onSuccess: (registration) => {
+    console.log('Service Worker registration successful');
+  },
+  onUpdate: (registration) => {
+    // Show update notification to user
+    const shouldUpdate = window.confirm(
+      'A new version of Joby is available! Would you like to update now?'
+    );
+    
+    if (shouldUpdate && registration.waiting) {
+      // Send message to service worker to skip waiting and activate new version
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      
+      // Reload all tabs to get the new version
+      window.location.reload();
+    }
+  },
+});
