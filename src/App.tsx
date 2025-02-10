5540bcc6ff033319d6eb85539565e80e65eab690
Kuria_byte <mwitumi21@gmail.com>
2025-02-10 19:26:03 +0300

    Create React PWA scafolding

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Joby</h1>
+        <p>Your new Progressive Web App is ready!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
